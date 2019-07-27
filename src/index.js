d8db74d2fe8c9a617dcbb41ea1cd5e1246f04808
Natalia Zakrevskaya <zakrevsn@gmail.com>
2019-07-27 13:23:47 +0200

    redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import reducer from "./reducer";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from "./serviceWorker";
+
+export const store = createStore(reducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <div className="gamebody">
+      <App />
+    </div>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
